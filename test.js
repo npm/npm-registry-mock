a636afed4f309aa54f9b098983d7d16cdae3b281
Ruy Adorno <ruyadorno@hotmail.com>
2019-09-12 20:01:57 -0400

    Custom registry vs package-lock resolved
    This fork of npm-registry-mock sets up a registry with a published
    version of underscore@1.3.3 that differs from that of the common
    registry enabling us to have a deterministic check to ensure a package
    was downloaded from the `--registry` server rather than the one
    specified in the resolved field of the package-lock.json file.

---
+++ test.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+const { readFileSync } = require('fs')
+const { exec } = require('child_process')
+const path = require('path')
+const tempdir = require('tempdir')
+const rimraf = require('rimraf')
+var mr = require('./index.js')
+
+const testFolder = path.join(__dirname, 'resolved-test')
+
+rimraf.sync(path.join(testFolder, 'node_modules'))
+
+mr({port: 1331}, function (err, s) {
+  exec(`npm install --cache ${tempdir.sync()} --registry http://localhost:1331 --no-audit`, { cwd: testFolder, env: process.env }, (err, stdout) => {
+    assert.equal(readFileSync(path.join(testFolder, 'node_modules/underscore/underscore.js')).toString().slice(0, 10), '//MODIFIED')
+    s.close()
+  })
+})
